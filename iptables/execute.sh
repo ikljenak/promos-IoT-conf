49a7aa2a827cb9ea97ebc6c0bc4d48602ab7b7e5
ikljenak <ikljenak.2@gmail.com>
2016-11-26 22:57:00 +0000

    iptables persistent

--- iptables/execute.sh
+++ iptables/execute.sh
@@ -1,6 +1,7 @@
 !#/bin/sh
 
 apt-get install iptables
+apt-get install iptables-persistent
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
