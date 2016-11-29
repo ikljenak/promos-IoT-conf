eabb56d5b4a2dc57fab4d14307fa4447da5464a0
ivankljenak <ivan@tiendanube.com>
2016-11-29 18:53:43 -0300

    global execution script

--- node/execute.sh
+++ node/execute.sh
@@ -5,3 +5,5 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 source ~ /.profile
 nvm install 6.9.1
 npm install
+npm install -g forever-service
+forever-service install --script index.js
